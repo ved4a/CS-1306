13d178b395430d6cfbd68e28a77f03b61501a48c
Vedaa Anand <work.vedaa@gmail.com>
2024-09-20 17:18:36 +0530

    feat: Create constants as foundation

--- a1/q8/decrypt.py
+++ a1/q8/decrypt.py
@@ -1,4 +1,8 @@
 import itertools, re, pyperclip
 import detectEnglish, vigenereCipher, frequencyAnalysis
 
-
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+MAX_KEY_LENGTH = 6 # Will not attempt keys longer than this.
+NUM_MOST_FREQ_LETTERS = 4 # Attempt this many letters per subkey.
+SILENT_MODE = False # If set to True, program doesn't print anything.
+NONLETTERS_PATTERN = re.compile('[^A-Z]')
