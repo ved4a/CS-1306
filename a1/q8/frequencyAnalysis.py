c98366ef84a045acc35b94f18b4edcb7536cac6f
Vedaa Anand <work.vedaa@gmail.com>
2024-09-20 16:10:15 +0530

    feat: Create file for frequency analysis

---
+++ a1/q8/frequencyAnalysis.py
@@ -0,0 +1,7 @@
+# The full list of letters ordered by frequency
+ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+
+# Alphabet
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# Returns count of how many times each letter appears in a message
