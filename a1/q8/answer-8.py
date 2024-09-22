fcbb61c17a779c245884ab1f38a545567d1ce167
Vedaa Anand <work.vedaa@gmail.com>
2024-09-22 17:48:36 +0530

    feat: Create main function

--- a1/q8/answer-8.py
+++ a1/q8/answer-8.py
@@ -6,3 +6,17 @@ MAX_KEY_LENGTH = 6 # Will not attempt keys longer than this.
 NUM_MOST_FREQ_LETTERS = 4 # Attempt this many letters per subkey.
 SILENT_MODE = False # If set to True, program doesn't print anything.
 NONLETTERS_PATTERN = re.compile('[^A-Z]')
+
+
+
+def main():
+    ciphertext = ""
+    hackedMessage = hackVigenere(ciphertext)
+
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
+
