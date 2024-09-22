8042ae9657210445aa6a37620cb724b9d95b4123
Vedaa Anand <work.vedaa@gmail.com>
2024-09-22 16:47:11 +0530

    feat: Setup skeleton structure

---
+++ a1/q3/answer-3-decrypt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+typedef struct {
+    char ch;
+    int index;
+} KeyCharIndex;
+
+int main(void)
+{
+    return 0;
+}
