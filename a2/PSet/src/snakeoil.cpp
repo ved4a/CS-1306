9419ac003056cf0cb3a7d17a3668eeaf6e71b94c
Vedaa Anand <work.vedaa@gmail.com>
2024-11-03 14:51:30 +0530

    fix: Remove Botan

--- a2/PSet/src/snakeoil.cpp
+++ a2/PSet/src/snakeoil.cpp
@@ -1,17 +1,7 @@
-/*
- * main.cpp
- *
- *  Created on: Jan 21, 2012
- *      Author: Michael Fischer
- *      Derived from code by Ewa Syta
- *      Solutions and edits by Debayan Gupta
- */
 #include <ctime>
 #include "crypto.hpp"
 
 int main(int argc, char* argv[]) {
-    Botan::LibraryInitializer init;
-
     try {
         unsigned int seed = time(NULL);
         Crypto toplevel(seed);
